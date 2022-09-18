6fb0622d124efcddb16b88795cbab53f24b95b88
Carter Reynolds <carmitrey@yahoo.com>
2022-09-18 04:25:16 -0400

    basic groundwork for assigning loot to Enemies (#3)
    still some work to do in management of items. will probably
    be better to convert some things to list and track quantities easier

--- classes/Enemies.py
+++ classes/Enemies.py
@@ -1,31 +1,76 @@
 from dictionaries.enemy_types import types, levels
 from dictionaries.enemy_names import bandit_names, goblin_names, undead_names, creature_names
+from dictionaries.loot import bandit_loot_dict, goblin_loot_dict, undead_loot_dict, creature_loot_dict
 import random as rand
 
 class Enemy:
     def __init__(self): # Only name and type will be required for now
         self.name = ""    # The name of the enemy
-        self.type = 0    # What type of enemy they are
-        self.level = 0      # The level of the enemy
+        self.type = ""    # What type of enemy they are
+        self.level = ""      # The level of the enemy
         self.health = 0     # How much health the enemy has
         self.defense = 0    # How well they can defend
         self.attack = 0     # How much damage they would do
         self.loot = []      # Loot they would drop, if any, will likely be randomly assigned
         self.weapon = ''    # Weapon they are using - should contribute to damage
         self.effect = []    # Random effect
-
+    
+            
+    def random_level(self):
+        return levels[rand.randint(0, 3)]
+    
+    def assign_random_loot(self, number_of_loot):
+        
+        for i in range(number_of_loot):
+            if self.type == types[0]:
+                random_loot = bandit_loot_dict[rand.randint(0, 8)]
+                self.loot.append(random_loot)
+            elif self.type == types[1]:
+                random_loot = goblin_loot_dict[rand.randint(0, 8)]
+                self.loot.append(random_loot)
+            elif self.type == types[2]:
+                random_loot = undead_loot_dict[rand.randint(0, 12)]
+                self.loot.append(random_loot)
+            elif self.type == types[3]:
+                random_loot = creature_loot_dict[rand.randint(0, 4)]
+                self.loot.append(random_loot)
+            else:
+                pass
+        
     # Make a random enemy with type of bandit
     def make_bandit(self):
+        
         self.name = bandit_names[rand.randint(0, 9)]
         self.type = types[0]
-        self.level = levels[rand.randint(0, 3)]
-        self.set_stats()
+        self.level = self.random_level()
+        
+        stats = self.set_stats()
+        
+        if self.level == levels[0]:
+            return self.assign_random_loot(2)
+        elif self.level == levels[1]:
+            return self.assign_random_loot(4)
+        elif self.level == levels[2]:
+            return self.assign_random_loot(6)
+        elif self.level == levels[3]:
+            return self.assign_random_loot(8)
+        
+        return stats
+            
+        
+        
     
     # Make a random enemy with type of goblin
     def make_goblin(self):
         self.name = goblin_names[rand.randint(0, 9)]
         self.type = types[1]
         self.level = levels[rand.randint(0, 3)]
+        
+        random_loot = goblin_loot_dict[rand.randint(0, 8)]
+        
+        for i in range(0, self.level):
+            self.loot.append(random_loot)
+            
         self.set_stats()
     
     # Make a random enemy with type of undead
@@ -33,6 +78,12 @@ class Enemy:
         self.name = undead_names[rand.randint(0, 2)]
         self.type = types[2]
         self.level = levels[rand.randint(0, 3)]
+        
+        random_loot = undead_loot_dict[rand.randint(0, 12)]
+        
+        for i in range(0, self.level):
+            self.loot.append(random_loot)
+        
         self.set_stats()
     
     # Make a random enemy with type of creature
@@ -40,44 +91,36 @@ class Enemy:
         self.name = creature_names[rand.randint(0, 2)]
         self.type = types[3]
         self.level = levels[rand.randint(0, 3)]
+        
+        random_loot = creature_loot_dict[rand.randint(0, 4)]
+        
+        for i in range(0, self.level):
+            self.loot.append(random_loot)
+            
         self.set_stats()
+    
+    
     
     # set up enemy stats based on type
     def set_stats(self):
+        
         # Set up all stats based on level
         if self.level == levels[0]:
             self.health = rand.randint(1, 10)
             self.defense = rand.randint(1, 10)
             self.attack = rand.randint(1, 10)
-            self.loot = rand.randint(0, 10)
-            self.weapon = rand.randint(0, 2) # Weak weapon  
         elif self.level == levels[1]:
             self.health = rand.randint(10, 20)
             self.defense = rand.randint(10, 20)
             self.attack = rand.randint(10, 20)
-            self.loot = [rand.randint(10, 20), rand.randint(0, 10)]
-            self.weapon = rand.randint(3, 5) # Average weapon
         elif self.level == levels[2]:
             self.health = rand.randint(20, 30)
             self.defense = rand.randint(20, 30)
             self.attack = rand.randint(20, 30)
-            self.loot = [
-                rand.randint(20, 30),
-                rand.randint(10, 20), 
-                rand.randint(0, 10)
-            ]
-            self.weapon = rand.randint(6, 8) # Strong Weapons
         elif self.level == levels[3]:
             self.health = rand.randint(30, 40)
             self.defense = rand.randint(30, 40)
             self.attack = rand.randint(30, 40)
-            self.loot = [   # 
-                rand.randint(30, 40),
-                rand.randint(20, 30),
-                rand.randint(10, 20), 
-                rand.randint(0, 10)
-            ]
-            self.weapon = rand.randint(9, 11) # Elite Weapons
         else:
             pass
       
---
+++ dictionaries/loot_descriptions.py
@@ -0,0 +1,76 @@
+
+# Bandit Loot Descriptions
+bandit_loot_descriptions = {
+    0: "A small pile of gold.",
+    1: "A small bottle of health potion. It's a bit cloudy.",
+    2: "A set of playing cards. They are in good condition.",
+    3: "A red bandana. It looks like it's been used to wipe blood off of a sword.",
+    4: "A long rope. It's about 10 feet long.",
+    5: "A sweaty pair of fingerless gloves. They smell not great.",
+    6: "A pair of leather boots. They look worn.",
+    7: "A pair of worn leather gloves.",
+    8: "A vial of thick-looking acid. It smells like rotten eggs.",
+    9: "A vial of poison."
+}
+
+special_bandit_loot_descriptions = {
+    0: "A token that claims you have defeated all bandits.",
+    1: "A bottle of wine owned by the 'Tavern Keeper'.",
+    2: "A bottle of whiskey owned by the 'Tavern Keeper'",
+    3: "A bottle of rum owned by the 'Tavern Keeper'"
+}
+
+# Goblin Loot Descriptions
+goblin_loot_descriptions = {
+    0: "A shiny gold coin",
+    1: "A small bottle of health potion. It's a bit cloudy.",
+    2: "A pile of decomposing fish heads. They smell awful.",
+    3: "A rusty sword. It looks like it's seen better days.",
+    4: "A rusty dagger. It looks like it's seen better days.",
+    5: "A peg leg. It looks like it's been used to hold a sword in place.",
+    6: "A coin purse. It's full of gold coins.",
+    7: "A jar of pickled toes. They look like they've been in there for a while.",
+    8: "A necklace made of teeth. It looks like it's been worn for a long time."
+}
+
+special_goblin_loot_descriptions = {
+    0: "A token that claims you have defeated all goblins.",
+    1: "A goblin's heart. It's still beating.",
+    2: "A cracked porcelain doll. It's missing an eye.",
+    3: "A pet squirrel. It looks like it's been well taken care of."
+}
+
+# Undead Loot Descriptions
+undead_loot_descriptions = {
+    0: "A dirty gold coin.",
+    1: "A small bottle of health potion. It's a bit cloudy.",
+    2: "A rusty sword. It looks like it's seen better days.",
+    3: "A rusty dagger. It looks like it's seen better days.",
+    4: "A tattered cloak. It looks like it was once well-made.",
+    5: "A tattered robe. It looks like it was once well-made.",
+    6: "A tattered shirt. It looks like it was once well-made.",
+    7: "A tattered pair of pants. They look like they've been worn for a while.",
+    8: "A tattered hat. It looks like it was once well-made.",
+    9: "A tattered pair of shoes. They look like they've been worn for a while.",
+    10: "A tattered pair of gloves. They look like they've been worn for a while."
+}
+
+special_undead_loot_descriptions = {
+    0: "A token that claims you have defeated all undead.",
+    1: "A vial of diseased spit. It's thick and viscous.",
+    2: "A small black pouch. It's full of gold coins."
+}
+
+# Creature Loot Descriptions
+creature_loot_descriptions = {
+    0: "A gold coin.",
+    1: "A small bottle of health potion. It's a bit cloudy.",
+    2: "A musty bag of spider webs.",
+    3: "A small bag of spider eggs. They look like they're about to hatch. Ew.",
+    4: "A small bag of spider legs. They look like they've been cooked. They smell awful."
+}
+
+special_creature_loot_descriptions = {
+    0: "A token that claims you have defeated all creatures.",
+    1: "A mystical spider eye. It's glowing."
+}
