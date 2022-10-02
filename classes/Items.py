96332c327dd4a53c6ea7af8ca7e94dc8d05f75bd
Carter Reynolds <carmitrey@yahoo.com>
2022-10-02 14:16:37 -0400

    inventory fixes

--- classes/Items.py
+++ classes/Items.py
@@ -1,6 +1,9 @@
 import loot_table
 from classes.Player import Player
 
+# Unsure for now if this class will be needed given the loot_table.py file
+# And items being stored in a dictionary to track quantities
+
 class Item:
     
     def __init__(self, name='', quantity=0):
