1fb9e263af6bf56bbe4d1ddc3546bddcb087c880
Carter Reynolds <carmitrey@yahoo.com>
2022-11-12 03:05:19 -0500

    testing autogenerate menu boxes using textbox.py

--- classes/MenuTexts.py
+++ classes/MenuTexts.py
@@ -1,4 +1,6 @@
 from termcolor import colored as _color
+from textbox import make_text_box
+from textwrap import dedent
 
 # This class is used to store all the text that is displayed in the menu
 # This class will eventually be used to automatically render windows based on the text and the size of the window
@@ -9,63 +11,28 @@ class Menu:
         pass
     
     @staticmethod
-    def display(menu, df=None):
+    def display(menu, inventory=None):
             
             if menu == 'help':
-                
-                print('**************************************************')
-                print('* pyRPG: A text-based RPG that no one asked for! *')
-                print('**************************************************')
-                print('*  help text here                                *')
-                print('*                                                *')
-                print('*                                                *')
-                print('*                                                *')
-                print('*                                                *')
-                print('*                                                *')
-                print('*                                                *')
-                print('*                  + Back (1) +                  *')
-                print('**************************************************')      
-            
+                make_text_box(title="", text="This is the help menu. You can use the arrow keys to navigate the menu." 
+                                             "Press enter to select an option. Press escape to exit the menu.", 
+                                             choices=['1. Back'])            
             elif menu == 'main':
                 
-                print('**************************************************')
-                print('*                  ~  pyRPG  ~                   *')
-                print('*     A text-based RPG that no one asked for!    *')
-                print('**************************************************')
-                print('*                                                *')
-                print('*     (1) - Start a new game                     *')
-                print('*     (2) - Load saved character                 *')
-                print('*     (3) - View the Help Menu                   *')
-                print('*                                                *')
-                print('*     (4) - Quit the game                        *')
-                print('*                                                *')
-                print('*                   Version: 0                   *')
-                print('**************************************************')
+                menu_text = "Select an option when you are ready to proceedccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+                make_text_box(title="Menu Menu:", text=menu_text, choices=['1. New Game', '2. Help', '3. Exit'])
 
             elif menu == 'pause':
-                    
-                print('**************************************************')
-                print('*                  ~  pyRPG  ~                   *')
-                print('*     A text-based RPG that no one asked for!    *')
-                print('**************************************************')
-                print('*                                                *')
-                print('*     (1) - View your inventory                  *')
-                print('*     (2) - Save the game                        *')
-                print('*     (3) - Return to the game                   *')
-                print('*                                                *')
-                print('*                                                *')
-                print('**************************************************')
+                make_text_box(title="", text="Pause Menu", choices=['1. View Inventory', '2. Save Game', '3. Return'])
                 
             elif (menu == 'inventory'):
-                
                 print(f"{_color('Items', 'red', attrs=['bold'])}  |  {_color('Quantity', 'yellow', attrs=['bold'])}")
                 
-                
-                for item in df: # This is no longer actually a dataframe, but a dictionary :shrug:
+                for item in inventory: 
                     
-                    quantity = df[item] #
+                    quantity = inventory[item] 
                     
-                    if quantity > 0: # Only display items that have a quantity greater than 0
+                    if quantity > 0: 
                         print(f"{_color(item, 'red')} {_color('x' + str(quantity), 'yellow')}")
                     else:
                         pass
