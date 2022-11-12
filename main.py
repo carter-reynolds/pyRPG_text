ba7280fdf2bab2537f571abaa8279ffad4e4686d
Carter Reynolds <carmitrey@yahoo.com>
2022-11-12 02:58:40 -0500

    clean up start game logic

--- main.py
+++ main.py
@@ -9,23 +9,20 @@ import setup
 def main():
     
     # Modes/Settings
-    RUNNING = True
-    MENU = True
+    GAME_RUNNING = True
+    IN_MENU = True
     SETUP = False
     PLAY = False
 
-    while RUNNING:
-        if MENU:
-            MENU = menu.main_menu()
-            if not MENU:
+    while GAME_RUNNING:
+        if IN_MENU:
+            IN_MENU = menu.main_menu()
+            if not IN_MENU:
                 while not SETUP:
                     if not setup.check_for_db():
-                        # if not, create it
                         setup.create_db()
                     else:  
-                        _setup = setup.game() # Returns a tuple: (Boolean, type<Player.object>)
-                        player = _setup[1]  # Created player object from setup
-                        _setup = _setup[0]  # Boolean whether or not setup was finished (overwrites the tuple, i know...)
+                        _setup, player = setup.game() # Returns a tuple: (Boolean, type<Player.object>)
                         if _setup == True:
                             SETUP = True
                         else:
