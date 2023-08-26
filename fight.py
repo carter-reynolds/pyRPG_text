1b7c68d0c44876a3f6e948d9b13729a1488c628f
carmitrey <carmitrey@yahoo.com>
2023-08-26 03:23:57 -0400

    cleaned up fight.py - logic rework

--- fight.py
+++ fight.py
@@ -3,166 +3,135 @@ import random as rand
 from classes.Utility import Utilities as util
 from classes.Text import textFunc as text
 from termcolor import colored as _color
+import classes._Player
 
-def fight(player):
+def fight(ply: classes._Player.Player):
     
-    """Fight a random enemy
-    
-    # TODO: This eventually needs massive clean up and refactoring
+    """Fight a random enemy based on the ply's level.
 
     Returns:
         None
     """
+
+    random_enemy_type = rand.randint(0, 3) 
     
-    RAN_AWAY = False # Used to break out of the fight loop
-    
-    random_type = rand.randint(0, 3) # Randomly choose an enemy type
-    
-    # Randomly choose an enemy level based on player level
-    if player.level == 0:
-        random_level = rand.randint(0, 1)
-    elif player.level == 1:
-        random_level = rand.randint(0, 2)
-    elif player.level == 2:
-        random_level = rand.randint(1, 3)
-    elif player.level == 3:
-        random_level = rand.randint(2, 3)
+    if ply.level == 0:
+        random_enemy_level = rand.randint(0, 1)
+    elif ply.level == 1:
+        random_enemy_level = rand.randint(0, 2)
+    elif ply.level == 2:
+        random_enemy_level = rand.randint(1, 3)
+    elif ply.level == 3:
+        random_enemy_level = rand.randint(2, 3)
     else:
         pass
+    
+    enemy = Enemy(random_enemy_type, random_enemy_level)
+    
+    PLAYER_RAN_AWAY = False
+    PLAYER_ATTACKING = True
+    ENEMY_ATTACKING = False
+    PLAYER_DEAD = False
+    ENEMY_DEAD = False
+    FIGHTING = True
+    
+    util.clear_term(0)
+    
+    message = ""
+                             
+    while FIGHTING:
+        
+        text.get_enemy_stats(enemy)
 
-    enemy = Enemy(random_type, random_level) # Create the enemy - set stats by type and level
-    
-    while not RAN_AWAY: # While the player has not run away
-        
-        util.clear_term(0)
-        
-        text.get_enemy_stats(enemy) # Print the enemy stats
-        
-        PLAYER_ATTACKING = True
-        ENEMY_ATTACKING = False
-        
-        if ENEMY_ATTACKING:
-            pass
-        else:
+        while ENEMY_ATTACKING and PLAYER_DEAD is False:
+            util.clear_term(0)
+            damage_taken = enemy.attack - ply.cur_defense
+            ply.cur_health -= damage_taken if (ply.cur_health - damage_taken) > 0 else ply.cur_health
             
-            message = ""
+            message = f"{enemy.name} attacks you for {damage_taken} damage!\nYou have {ply.cur_health} health left!" \
+                if (ply.cur_health - damage_taken) > 0 \
+                    else ""
             
-            while PLAYER_ATTACKING:
-                
-                attack_actions = ["attack", "hit", "fight", "kill", "run", "heal"]
-                
+            if ply.cur_health == 0:
                 util.clear_term(0)
-                
-                text.get_enemy_stats(enemy) # Print the enemy stats
-                util.spacing(1)
-                text.get_cur_stats(player) # Print the player stats
-                
-                util.spacing(1)
-                
-                print(message, '\n')
-                
-                action = input("What do you want to do?\n> ")
-                action = action.lower()
-                
-                if action in attack_actions: 
-                    if action != 'run':
-                        if action == 'heal':
-                            player_try_heal = player.use_health_potion() # Try to heal the player
-                            
-                            # The above returns a tuple: (success bool, message)
-                            player_did_heal = player_try_heal[0] # The first value is a boolean that tells us if the player was able to heal
-                            message = player_try_heal[1] # The second value is a string that tells us what happened
-                            continue
-                        else:
-                            pass
-                    elif action == 'run':
-                        print("You ran away!")
-                        PLAYER_ATTACKING = False
-                        RAN_AWAY = True
-                        break
-                    
-                    
-                    damage = player.cur_attack 
-                
-                    player_damage_done = enemy.take_damage(damage) 
-                
-                    if not player_damage_done[0]: # If damage was not done, print the reason and take damage
-                        player.cur_health -= (enemy.attack - player.cur_defense)
-                        message = player_damage_done[1]
-                        
-                    else:
-                        
-                        message = player_damage_done[1] # If damage was done, print the message
-                        player.alter_stamina(5, 0) # Decrease stamina by 5 since we attacked
-                        
-                        if enemy._defeated():
-                            util.clear_term(0)
-                            util.scroll_text(_color(f'You defeated {enemy.name}!\n', 'green'), 0.05)
-                            PLAYER_ATTACKING = False
-                            ENEMY_ATTACKING = False
-                            RAN_AWAY = False
-                            break
-                        else:
-                            continue
-                else:
-                    continue            
-            
-        if RAN_AWAY == True:
-            LOOTING = False                 
-        else: 
-            LOOTING = True
-        
-        while LOOTING:
-            action = input("Do you want to loot the enemy?\n> ")
-            
-            loot_actions = ["yes", "y", "loot", "take"]
-            
-            if action in loot_actions:
-                print("You loot the enemy!")
-                player.gold += enemy.gold
-                
-                util.clear_term(2, f'You gained {str(enemy.gold)} gold!')
-                util.clear_term(2, "You found the following items:\n")
-                
-                # TODO: Player still needs to gain experience from defeating an enemy
-                
-                for item in enemy.loot:
-                    
-                    print('\u25BA ' + item + '\n')
-                    
-                    # Check for the item already existing in the player's inventory dictionary
-                    if item in player.inventory.keys():
-                        player.inventory[item] += 1 # If it does, increment the value by 1
-                    else:
-                        '''
-                            The following conditional statement shouldn't ever run unless the player reloads a game
-                            and we added a new object to the game, it will add the new object to the player's inventory
-                            when they loot the enemy and find the new item.
-                        '''
-                        player.inventory[item] = 1 # If it doesn't, add it to the dictionary with a value of 1 
-                        
-                input("Press enter to continue...")
-                
-                if input:             
-                    LOOTING = False
-                    break
+                util.scroll_text(_color(f'{enemy.name} defeated you!\n', 'red'), 0.05)
+                ENEMY_ATTACKING = False
+                PLAYER_DEAD     = True
+                break
             else:
-                print("You leave the enemy alone.")
-                LOOTING = False
-                
-        return None
+                ENEMY_ATTACKING  = False
+                PLAYER_ATTACKING = True
+                continue
             
+        if PLAYER_DEAD:
+            FIGHTING = False
+            continue
+
+        while (PLAYER_ATTACKING and ENEMY_DEAD is False):
+            
+            attack_actions = ["attack", "hit", "fight", "kill", "run", "heal"]
+            
+            util.clear_term(0)
+            
+            text.get_enemy_stats(enemy) # Print the enemy stats
+            util.spacing(1)
+            text.get_cur_stats(ply) # Print the ply stats
+            util.spacing(1)
+            
+            print(message, '\n')
+            
+            action = input("What do you want to do?\n> ")
+            action = action.lower()
+            
+            if action in attack_actions:
+                
+                if action == 'run':
+                    print("You ran away!")
+                    FIGHTING = False
+                    break
+                
+                if action == 'heal':
+                    player_try_heal, message = ply.use_health_potion() # Try to heal the ply
+                    continue
+                else:
+                    damage = ply.cur_attack 
+                    player_damage_done, message = enemy.take_damage(damage) 
+                
+                    if not player_damage_done:
+                        ply.cur_health -= (enemy.attack - ply.cur_defense)
+                    else:
+                        ply.cur_stamina -= 5
+                    
+                    if enemy._defeated():
                         
-                   
-                
-                
-                
-                
-    
+                        coin_string = '\u26C1' * enemy.gold
+                        util.clear_term(0)
+                        util.scroll_text(_color(f'You defeated {enemy.name}!\n', 'green'), 0.05)
+                        util.clear_term(2)
+                        util.scroll_text(f"You have gained {enemy.gold} gold and {10 * enemy.level} xp!\n", 0.05)
+                        util.scroll_text(_color(coin_string, 'yellow'), 0.05)
+                        util.clear_term(2, "You found the following items:\n")
+                        
+                        ply.gold += enemy.gold
+                        ply.xp += 10 * enemy.level if enemy.level > 0 else 1
+                        ply.level_check()
+                        
+                        ENEMY_DEAD = True
+                        FIGHTING = False
+                        
+                        for item in enemy.loot:
+                            print(f'\u25BA {item}\n')
+                            if item in ply.inventory.keys():
+                                ply.inventory[item] += 1
+                            else:
+                                ply.inventory[item] = 1
+                                continue  
+                    else:
+                        ENEMY_ATTACKING = True
+                        PLAYER_ATTACKING = False
+                        continue
+            else:
+                continue            
         
-         
-    
-    
-    
-    
-    
+    input("Press enter to continue...")   
+    return None
